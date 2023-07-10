135b95ad487fd7f312778d64a2a06f718fecbb0f
David Evans <dwe@backpainclinic.co.uk>
2023-07-10 11:01:07 +0100

    Update

---
+++ boot_EFA.R
@@ -0,0 +1,332 @@
+# Load required packages
+library(readr)
+library(psy)
+library(psych)
+library(lavaan)
+library(tidyverse)
+library(GPArotation)
+library(rstudioapi)
+library(boot)
+library(EFA.dimensions)
+library(EFAtools)
+
+# Set seed
+set.seed(123)
+
+# Set number of bootstrap iterations
+boot_iterations <- 100 # Ensure this is 1000 for actual analysis
+
+# Set cutoff for factor loadings
+threshold <- 0.3
+
+# Set the file path
+path <- "/Users/davidevans/Library/CloudStorage/OneDrive-Personal/My Projects/UBham/Disability attribution/Analyses/Factor analysis/ADLInterferenceAndAt-UniversalUnattribute_DATA_2023-06-21_1216.csv"
+
+# Import data from csv
+data <- read_csv(path)
+
+# Get questionnaire variables (retain all variables except 'record_id')
+questionnaire_vars <- names(data)[!(names(data) %in% "record_id")]
+
+# Count the number of questionnaire variables
+num_vars <- length(questionnaire_vars)
+print(num_vars)
+
+# Remove rows (cases) with more than 2 missing values in questionnaire items
+data <- data[rowSums(is.na(data[, questionnaire_vars])) <= 2, ]
+
+# Replace missing values with column mean for rows (cases) with 2 or fewer missing values
+data <- mutate(data, across(all_of(questionnaire_vars), ~ifelse(is.na(.), mean(., na.rm = TRUE), .), .names = "{.col}"))
+
+# Count the number of subjects (rows)
+num_subjects <- nrow(data)
+print(num_subjects)
+
+# Calculate the minimum number of subjects required per dataset (EFA and CFA)
+min_subjects <- num_vars * 20  # Minimum of 20 subjects per item for EFA
+
+# Calculate the split ratio if/when splitting for EFA:CFA (if not splitting, set to 1.0)
+split_ratio <- min_subjects / num_subjects
+print(paste("The minimum split ratio is", split_ratio))
+
+# Check if split ratio is less than 0.5 (assuming at least 50% of the data for EFA)
+if (split_ratio < 0.5) {
+  print(paste("The split ratio has been increased from", split_ratio, "to 0.5"))
+  split_ratio <- 0.5
+} else if (split_ratio > 0.8) {  # too many items for the number of subjects
+  print(paste("Too few available subjects for the number of items to spilt the data."))
+  split_ratio <- 1.0
+}
+print(paste("The split ratio has been set to", split_ratio))
+
+# Split the data into two subsets: one for EFA and one for CFA
+data_ids <- data$record_id
+split_size <- ceiling(length(data_ids) * split_ratio) # use 'ceiling' to round up to an integer
+split_ids <- sample(data_ids, size = split_size)
+efa_data <- data[data$record_id %in% split_ids, ]
+cfa_data <- data[!(data$record_id %in% split_ids), ]
+
+# Function to get number of factors based on Parallel Analysis (PA)
+get_nfactors_pa <- function(data, indices) {
+  bootstrap_data <- data[indices, ]  # Create the bootstrap sample
+  pa <- psych::fa.parallel(bootstrap_data, fm = "minres", plot = FALSE)
+  nfactors_pa <- pa$nfact
+  return(nfactors_pa)
+}
+
+# Function to get number of factors based on Comparative Data (CD)
+get_nfactors_cd <- function(data, indices) {
+  bootstrap_data <- data[indices, ]  # Create the bootstrap sample
+  cd <- EFAtools::CD(bootstrap_data, n_factors_max = NA, N_pop = 10000, N_samples = 500, 
+                     alpha = 0.3, use = "pairwise.complete.obs", 
+                     cor_method = "pearson", max_iter = 50)
+  nfactors_cd <- cd$n_factors
+  return(nfactors_cd)
+}
+
+# Function to get number of factors based on Minimum Average Partial (MAP) criterion
+get_nfactors_map <- function(data, indices) {
+  bootstrap_data <- data[indices, ]  # Create the bootstrap sample
+  vss_results <- psych::VSS(bootstrap_data, rotate = "none", fm = "minres", plot = FALSE)
+  nfactors_map <- which.min(vss_results$map)
+  return(nfactors_map)
+}
+
+# Function to get number of factors based on Eigenvalues (and store eigenvalues)
+get_nfactors_eigen <- function(data, indices) {
+  bootstrap_data <- data[indices, ]  # Create the bootstrap sample
+  correlation_matrix <- cor(bootstrap_data)
+  eigenvalues <- eigen(correlation_matrix)$values
+  nfactors_eigen <- sum(eigenvalues > 1)  # Keep eigenvalues > 1
+  return(c(nfactors_eigen, eigenvalues))  # Return a vector
+}
+
+# Perform bootstrap with each of the four methods
+boot_results_pa <- boot(data = efa_data[, questionnaire_vars], statistic = get_nfactors_pa, R = boot_iterations)
+boot_results_cd <- boot(data = efa_data[, questionnaire_vars], statistic = get_nfactors_cd, R = boot_iterations)
+boot_results_map <- boot(data = efa_data[, questionnaire_vars], statistic = get_nfactors_map, R = boot_iterations)
+boot_results_eigen <- boot(data = efa_data[, questionnaire_vars], statistic = get_nfactors_eigen, R = boot_iterations)
+
+# Extract the number of factors and eigenvalues from the boot_results_eigen
+boot_nfactors_eigen <- boot_results_eigen$t[, 1]  # The first element is the number of factors
+boot_eigenvalues <- boot_results_eigen$t[, -1]  # The remaining elements are the eigenvalues
+
+# Calculate the mean and 95% confidence interval for each eigenvalue
+mean_eigenvalues <- colMeans(boot_eigenvalues, na.rm = TRUE)
+ci_eigenvalues <- apply(boot_eigenvalues, 2, function(x) quantile(x, probs = c(0.025, 0.975), na.rm = TRUE))
+
+# Create a data frame for plotting
+plot_data <- data.frame(
+  eigenvalue_index = 1:length(mean_eigenvalues),
+  mean_eigenvalue = mean_eigenvalues,
+  ci_lower = ci_eigenvalues[1, ],
+  ci_upper = ci_eigenvalues[2, ]
+)
+
+# Create a scree plot with 95% confidence intervals
+df <- data.frame(nfactors = boot_nfactors_eigen)
+ggplot(plot_data, aes(x = eigenvalue_index, y = mean_eigenvalue)) +
+  geom_line() +
+  geom_point() +
+  geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper), width = 0.2) +
+  labs(
+    title = "Scree Plot with Bootstrap Eigenvalues",
+    x = "Eigenvalue Index",
+    y = "Mean Eigenvalue"
+  ) +
+  theme_minimal()
+
+# Function to create histogram
+plot_histogram <- function(nfactors, title) {
+  df <- data.frame(nfactors = nfactors)
+  
+  ggplot(df, aes(x=nfactors)) +
+    geom_histogram(binwidth=1, fill="skyblue", color="black") +
+    labs(title=paste("Histogram of Bootstrapped Results (", title, ")"),
+         x="Number of Factors",
+         y="Frequency") +
+    scale_x_continuous(breaks = seq(floor(min(df$nfactors)), ceiling(max(df$nfactors)), by = 1))  # Ensure x-axis only displays integers
+}
+
+# Plot histograms of bootstrapped results
+plot_histogram(boot_results_pa$t, "Parallel Analysis")
+plot_histogram(boot_results_cd$t, "Comparative Data")
+plot_histogram(boot_results_map$t, "Minimum Average Partial")
+plot_histogram(boot_nfactors_eigen, "Eigenvalues > 1")
+
+# Get mode (or median) of bootstrap estimates for number of factors
+nfactors_final_pa <- as.integer(names(which.max(table(boot_results_pa$t))))  # or median(boot_results_pa$t)
+nfactors_final_cd <- as.integer(names(which.max(table(boot_results_cd$t))))  # or median(boot_results_cd$t)
+nfactors_final_map <- as.integer(names(which.max(table(boot_results_map$t))))  # or median(boot_results_map$t)
+nfactors_final_eigen <- as.integer(names(which.max(table(boot_results_eigen$t))))  # or median(boot_results_eigen$t)
+
+# Print out the results with a descriptive message
+print(paste("Based on the bootstrapped PA criterion, the modal number of factors is", nfactors_final_pa))
+print(paste("Based on the bootstrapped CD criterion, the modal number of factors is", nfactors_final_cd))
+print(paste("Based on the bootstrapped MAP criterion, the modal number of factors that minimise the average squared partial correlation is", nfactors_final_map))
+print(paste("Based on the bootstrapped Eigenvalue > 1 criterion, the modal number of factors for Eigenvalues > 1 is", nfactors_final_eigen))
+
+# Make final decision on number of factors by asking for user input
+nfactors_input <- readline(prompt="How many factors do you want to retain for the EFA? ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Part 2: Run EFA
+
+# Set number of factors for EFA
+nfactors <- as.integer(nfactors_input)
+print(paste("The number of factors to be retained for the EFA is", nfactors))
+
+# Helper function to perform EFA
+run_factor_analysis <- function(data, nfactors) {
+  if(nfactors == 1) {
+    rotate_method = 'none'
+  } else {
+    rotate_method = 'oblimin'
+  }
+  
+  fa_result <- tryCatch({
+    psych::fa(r = data, nfactors = nfactors, rotate = rotate_method, 
+              scores = "regression", fm = "minres")
+  }, error = function(e) {
+    print(paste("EFA failed:", e$message))
+    return(NULL)
+  })
+  print(fa_result)
+  return(fa_result)
+}
+
+# Function to perform Procrustes rotation of loadings from the bootstrap EFA to match loadings of the initial EFA
+procrustes_rotate_loadings <- function(bootstrap_efa, initial_efa) {
+  procrustes_result <- tryCatch({
+    EFA.dimensions::PROCRUSTES(loadings = bootstrap_efa$loadings, target = initial_efa$loadings, type = 'oblique', verbose = FALSE)
+  }, error = function(e) {
+    print(paste("Procrustes rotation failed:", e$message))
+    return(NULL)
+  })
+  if(!is.null(procrustes_result)){
+    return(procrustes_result$loadingsPROC)
+  } else {
+    return(NULL)
+  }
+}
+
+# Function to perform EFA on a bootstrap sample, rotate loadings and return them in vector form
+efa_func <- function(data, indices, nfactors, initial_efa) {
+  bootstrap_data <- data[indices, ]
+  
+  bootstrap_efa <- run_factor_analysis(bootstrap_data, nfactors)
+  
+  if (is.null(bootstrap_efa)) return(list(rotated = NULL, unrotated = NULL))
+  
+  # Remove NA rows from loadings matrix before converting to data frame
+  bootstrap_efa$loadings <- bootstrap_efa$loadings[rowSums(is.na(bootstrap_efa$loadings)) != ncol(bootstrap_efa$loadings), ]
+  
+  unrotated_loadings_df <- as.data.frame(bootstrap_efa$loadings)
+  unrotated_loadings_df$item <- colnames(data)
+  print("Iteration loadings after EFA")
+  print(unrotated_loadings_df)
+  
+  if(nfactors == 1) {
+    loadings_df <- as.data.frame(bootstrap_efa$loadings)
+    print("Iteration final loadings")
+  } else {
+    rotated_loadings <- procrustes_rotate_loadings(bootstrap_efa, initial_efa)
+    if (is.null(rotated_loadings)) return(list(rotated = NULL, unrotated = unrotated_loadings_df))
+    
+    loadings_df <- as.data.frame(rotated_loadings)
+    print("Iteration final loadings after Procrustes rotation")
+  }
+  
+  loadings_df$item <- colnames(data)
+  print(loadings_df)
+  
+  return(list(rotated = loadings_df, unrotated = unrotated_loadings_df))
+}
+
+# Function to perform bootstrap EFA
+run_bootstrap_efa <- function(efa_data, questionnaire_vars, boot_iterations, nfactors, initial_efa) {
+  results_list_rotated <- vector("list", boot_iterations)
+  results_list_unrotated <- vector("list", boot_iterations)
+  
+  for (i in 1:boot_iterations) {
+    print(paste("Bootstrap EFA iteration", i))
+    
+    result <- efa_func(efa_data[, questionnaire_vars], sample(1:nrow(efa_data), replace = TRUE), nfactors, initial_efa)
+    
+    if (!is.null(result$rotated)) {
+      results_list_rotated[[i]] <- result$rotated
+    }
+    if (!is.null(result$unrotated)) {
+      results_list_unrotated[[i]] <- result$unrotated
+    }
+  }
+  return(list(rotated = results_list_rotated, unrotated = results_list_unrotated))
+}
+
+# Function returning summary data frame of bootstrap EFA results
+summarize_loadings <- function(results_list) {
+  loadings_df <- do.call(rbind, results_list)
+  
+  loadings_summary <- loadings_df %>%
+    gather(key = "factor", value = "loading", -item) %>%
+    group_by(item, factor) %>%
+    summarise(mean = mean(loading, na.rm = TRUE), 
+              ci_lower = quantile(loading, 0.025, na.rm = TRUE),
+              ci_upper = quantile(loading, 0.975, na.rm = TRUE),
+              .groups = 'drop') %>%
+    mutate(Mean_CI = sprintf("%.2f (%.2f, %.2f)", mean, ci_lower, ci_upper))
+  
+  return(loadings_summary)
+}
+
+# Function to pivot factor loadings summary to wide format
+pivot_loadings <- function(loadings_summary) {
+  loadings_summary_wide <- loadings_summary %>%
+    select(-c(mean, ci_lower, ci_upper)) %>%
+    spread(key = factor, value = Mean_CI)
+  
+  return(loadings_summary_wide)
+}
+
+# Function to prepare loading summaries and print tables
+prepare_and_print_loadings <- function(loadings_summary, print_title) {
+  loadings_summary_wide <- pivot_loadings(loadings_summary)
+  
+  # Order rows on descending values within first column
+  loadings_summary_wide <- loadings_summary_wide %>% arrange(desc(!!sym(names(loadings_summary_wide)[2])))
+  
+  print(print_title)
+  print(loadings_summary_wide)
+}
+
+# Run initial EFA to create factor structure
+initial_efa <- run_factor_analysis(efa_data[, questionnaire_vars], nfactors)
+if(is.null(initial_efa)){
+  print("Initial EFA failed. Check your data.")
+} else {
+  print(initial_efa)
+  
+  bootstrap_results <- run_bootstrap_efa(efa_data, questionnaire_vars, boot_iterations, nfactors, initial_efa)
+  
+  loadings_summary_rotated <- summarize_loadings(bootstrap_results$rotated)
+  loadings_summary_unrotated <- summarize_loadings(bootstrap_results$unrotated)
+  
+  # Print summaries
+  prepare_and_print_loadings(loadings_summary_rotated, "Final loadings summary after Procrustes rotations")
+  prepare_and_print_loadings(loadings_summary_unrotated, "Loadings summary without Procrustes rotations")
+}
+
+
